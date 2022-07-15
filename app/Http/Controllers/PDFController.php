042034508f019ecfd39add0f1d4fcef18a1332e1
Theint-Haymann-Hnin <scm.theinthaymannhnin@gmail.com>
2022-07-15 08:32:56 +0630

    first commit

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use PDF;
+use Mail;
+
+use Illuminate\Http\Request;
+
+class PDFController extends Controller
+{
+    public function index()
+    {
+        $data["email"] = "scm.theinthaymannhnin@gmail.com";
+        $data["title"] = "From TheintHaymannHnin.com";
+        $data["body"] = "This is Demo";
+  
+        $pdf = PDF::loadView('emails.myTestMail', $data);
+  
+        Mail::send('emails.myTestMail', $data, function($message)use($data, $pdf) {
+            $message->to($data["email"], $data["email"])
+                    ->subject($data["title"])
+                    ->attachData($pdf->output(), "text.pdf");
+        });
+  
+        dd('Mail sent successfully');
+    }
+}
